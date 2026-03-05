18e54837ebbb9451b44e583f07e97779c4a471d8
farolee <farolee@gmail.com>
2026-03-05 16:49:18 -0500

    commit Mod Godfrey

---
+++ Godfrey.py
@@ -0,0 +1,16 @@
+ Create 4 Subnets with the following Parameter:
+    - Subnet 1 Name: VPC C Public Subnet 1
+    - Availability Zone: us-east-1a
+    - CIDR Block: 10.2.0.0/24
+
+    - Subnet 2 Name: VPC C Public Subnet 2
+    - Availability Zone: us-east-1b
+    - CIDR Block: 10.2.1.0/24
+
+    - Subnet 3 Name: VPC C Private Subnet 1
+    - Availability Zone: us-east-1a
+    - CIDR Block: 10.2.2.0/24
+
+    - Subnet 4 Name: VPC C Private Subnet 2
+    - Availability Zone: us-east-1b
+    - CIDR Block: 10.2.3.0/24
